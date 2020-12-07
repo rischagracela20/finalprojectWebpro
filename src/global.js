674e3351fe80ee3406eb27960346d32ba4cb27df
rischagracela20 <https://github.com/rischagracela20/Webpro2020-S11710098.git>
2020-12-07 14:23:14 +0700

    API global

--- src/global.js
+++ src/global.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React,{useState, useEffect} from 'react'
+import axios from 'axios'
 
 const Global = () => {
+    const [world, setWorld] = useState({confirm:NaN,death:NaN,recovered:NaN})
+
+  useEffect(() => {
+    axios
+      .get("https://covid19.mathdro.id/api")
+      .then((res) => {
+        setWorld({
+          confirm:res.data.confirmed.value,
+          death:res.data.deaths.value,
+          recovered:res.data.recovered.value
+        });
+      })
+      .catch((err) => console.log(err));
+  });
   return (
     <div>
-      Global
+     <h2>Global</h2>
+  <h5>Confirm: {world.confirm}</h5>
+  <h5>Death: {world.death}</h5>
+  <h5>Recovered: {world.recovered}</h5>
     </div>
   );
 }
